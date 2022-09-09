5c5e91d422f0024abbf34796ca2ca6aa6d168d1f
arep01 arep01 <arep01@labinfo.is.escuelaing.edu.co>
2022-09-09 12:43:08 -0500

    c12

---
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -0,0 +1,71 @@
+package edu.escuelaing.arep;
+
+import java.net.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.*;
+
+public class HttpServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(getPort());
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        boolean flag = true;
+        while (flag) {
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            Path file = Paths.get("resources" + "index.html");
+            Charset charset = Charset.forName("UTF-8");
+            String outmsg = "";
+            outputLine =
+                    "HTTP/1.1 200 OK\r\n"
+                            + "Content-Type: text/html\r\n"
+                            + "\r\n";
+            try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                    outmsg += "\r\n" + line;
+                }
+            } catch (IOException x) {
+                System.err.format("IOException: %s%n", x);
+            }
+            outputLine += outmsg;
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+    public static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; 
+    }
+}
